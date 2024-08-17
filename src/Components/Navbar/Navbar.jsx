2d35bb6193343ad8f7669e175eabca8036d95583
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-17 16:42:31 +0600

    working on navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+import logo from "/wood.jpg";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <a>Item 1</a>
+      </li>
+      <li>
+        <a>Item 3</a>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar lg:px-24 text-white bg-[#795548]">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <div className=" flex items-center gap-4">
+          <img className="h-12 rounded-full" src={logo} alt="" />
+          <div>
+            <h4 className="text-3xl font-bold font-rancho">Woodify</h4>
+          </div>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar  navbar-end">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn btn-ghost btn-circle avatar"
+        >
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS Navbar component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rancho: ['"Rancho", cursive'],
+        // Add more custom font families as needed
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
+
+// font-family: ;
